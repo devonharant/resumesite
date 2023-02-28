28884cbe588309351d03c9e83b6f5c23b948865f
devonharant <devonharant@Devons-MBP.attlocal.net>
2023-02-28 09:22:15 -0500

    trying again flex issues about me

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -17,18 +17,19 @@ export default function AboutMe() {
                 </AccordionSummary>
                 <AccordionDetails style={{width: '95%'}}>
                     <Box display="flex" flexDirection="row" justifyContent="space-between" flexWrap='wrap'>
-                        <img src={gamecock} alt="cocky" style={{height: 100}}/>
-                        <img src={header} alt="head" style={{height: 100}}/>
-                        <img src={deadhead} alt="syf" style={{height: 100}}/>
+                        <img src={gamecock} alt="cocky" style={{height: 75}}/>
+                        <img src={header} alt="head" style={{height: 75}}/>
+                        <img src={deadhead} alt="syf" style={{height: 75}}/>
+                    
+                        <Typography textAlign="left">
+                            I am currently a Software Engineer, Consultant at Booz Allen Hamilton. 
+                            I graduated from the University of South Carolina with a BS in Computer Science. 
+                            I am also a reservist in the Marine Corps as a Light Armored Vehicle Marine. 
+                            As a software engineer at Booz Allen Hamilton, I am a full-stack engineer utilizing JavaScript, SQL, and various related frameworks and languages. 
+                            As a Marine, I am responsible for operating and maintaining the turret system of the LAV25.
+                            Some fun facts about me, I collect vinyl, Legos, and am an avid Dead Head!
+                        </Typography>
                     </Box>
-                    <Typography textAlign="left">
-                    I am currently a Software Engineer, Consultant at Booz Allen Hamilton. 
-                    I graduated from the University of South Carolina with a BS in Computer Science. 
-                    I am also a reservist in the Marine Corps as a Light Armored Vehicle Marine. 
-                    As a software engineer at Booz Allen Hamilton, I am a full-stack engineer utilizing JavaScript, SQL, and various related frameworks and languages. 
-                    As a Marine, I am responsible for operating and maintaining the turret system of the LAV25.
-                    Some fun facts about me, I collect vinyl, Legos, and am an avid Dead Head!
-                    </Typography>
                 </AccordionDetails>
             </Accordion>
         </>
