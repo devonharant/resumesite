5efc124dd5b9b9742785f121d9da4dd9e6ca24a9
devonharant <devonharant@MacBookPro-468.lan>
2026-02-04 10:47:09 -0500

    updating counter component

--- src/Components/ButtonIncrement.jsx
+++ src/Components/ButtonIncrement.jsx
@@ -1,6 +1,8 @@
+import { yupResolver } from "@hookform/resolvers/yup";
 import { Button, Typography, Box, TextField } from "@mui/material";
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
+import * as yup from "yup";
 
 export default function ButtonIncrement() {
   const [count, setCount] = useState(0);
@@ -8,7 +10,44 @@ export default function ButtonIncrement() {
   const MAX_LIMIT = 9999999;
   const MAX_LIMIT_BOTTOM = -9999999;
 
-  const { submit, formState } = useForm();
+  const schema = yup.object({
+    multiply: yup
+      .number()
+      .transform((value, originalValue) =>
+        originalValue === "" ? null : value,
+      )
+      .max(9999, "Too big a number!")
+      .min(-9999, "Too small a number!")
+      .typeError("Must be a number!")
+      .nullable(),
+    divide: yup
+      .number()
+      .transform((value, originalValue) =>
+        originalValue === "" ? null : value,
+      )
+      .test("is-zero", "Cannot divide by zero!", (value) => value !== 0)
+      .max(9999, "Too big a number!")
+      .min(-9999, "Too small a number!")
+      .typeError("Must be a number!")
+      .nullable(),
+  });
+  const defaultValues = {
+    multiply: "",
+    divide: "",
+  };
+
+  const {
+    handleSubmit,
+    formState: { errors },
+    control,
+    reset: resetForm,
+    setError,
+    watch,
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onChange",
+    defaultValues,
+  });
 
   const countUp = () => {
     setCount((prev) => {
@@ -23,37 +62,112 @@ export default function ButtonIncrement() {
     });
   };
 
-  const multiplyBy = (values) => {};
+  const watchMultiply = watch("multiply");
+  const watchDivide = watch("divide");
 
-  const divideBy = (values) => {};
+  const isMultiplyDisabled = !!watchDivide && watchDivide !== "";
+  const isDivideDisabled = !!watchMultiply && watchMultiply !== "";
 
+  const multiplyBy = (value) => {
+    setCount((prev) => {
+      const result = prev * value;
+      return result <= MAX_LIMIT_BOTTOM || result >= MAX_LIMIT ? prev : result;
+    });
+  };
+
+  const divideBy = (value) => {
+    setCount((prev) => {
+      const result = prev / value;
+      return result <= MAX_LIMIT_BOTTOM || result >= MAX_LIMIT ? prev : result;
+    });
+  };
+
+  const executeOperation = (values) => {
+    if (
+      values.multiply !== "" &&
+      values.multiply !== null &&
+      !isMultiplyDisabled
+    ) {
+      multiplyBy(Number(values.multiply));
+    } else if (
+      values.divide !== "" &&
+      values.divide !== null &&
+      !isDivideDisabled
+    ) {
+      divideBy(Number(values.divide));
+    }
+    resetForm();
+  };
   const reset = () => {
+    resetForm();
     setCount(0);
   };
 
   return (
     <Box display="flex" flexDirection="column" sx={{ paddingBottom: "12px" }}>
-      <Box display="flex" flexDirection="row">
-        <Button onClick={countDown} style={{ color: "red" }}>
+      <Box display="flex" flexDirection="row" gap={2}>
+        <Button
+          onClick={countDown}
+          style={{ color: "red", border: "1px solid red" }}
+        >
           Count Down
         </Button>
-        <Typography style={{ paddingTop: "6px" }}>{count}</Typography>
-        <Button onClick={countUp} style={{ color: "green" }}>
+        <Button
+          onClick={countUp}
+          style={{ color: "green", border: "1px solid green" }}
+        >
           Count Up
         </Button>
-        <Button onClick={reset}>Reset</Button>
+        <Button onClick={reset} style={{ border: "1px solid" }}>
+          Reset Count
+        </Button>
+        <Typography style={{ paddingTop: "6px" }}>COUNT: {count}</Typography>
       </Box>
-      <Box display="flex" flexDirection="row">
-        <TextField
-          label="Multiply By"
-          variant="standard"
-          className="custom-green-textfield"
+      <Box
+        display="flex"
+        alignItems="flex-end"
+        flexDirection="row"
+        sx={{ paddingTop: "1rem" }}
+        gap={2}
+      >
+        <Controller
+          name="multiply"
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <TextField
+              label="Multiply By"
+              variant="standard"
+              className="custom-green-textfield"
+              value={value}
+              onChange={onChange}
+              error={errors.multiply}
+              helperText={errors.multiply ? errors.multiply.message : null}
+            />
+          )}
         />
-        <TextField
-          label="Divide By"
-          variant="standard"
-          className="custom-green-textfield"
+
+        <Controller
+          name="divide"
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <TextField
+              label="Divide By"
+              variant="standard"
+              className="custom-green-textfield"
+              value={value}
+              onChange={onChange}
+              error={errors.divide}
+              helperText={errors.divide ? errors.divide.message : null}
+            />
+          )}
         />
+
+        <Button
+          onClick={handleSubmit(executeOperation)}
+          style={{ color: "green", border: "1px solid green" }}
+        >
+          EXECUTE
+        </Button>
       </Box>
     </Box>
   );
